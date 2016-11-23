92c5da96cc785a788ee560e12e3988f0112700b2
Paul Kramme <pjkramme@gmail.com>
2016-11-23 10:35:24 +0100

    Add fixes and template

--- wscreator.py
+++ wscreator.py
@@ -3,18 +3,29 @@
 import os
 import sys
 
-def main():
-	name = sys.argv[1]
-	templatepath = sys.argv[2]
-	contentpath = sys.argv[3]
+
+def writetofile(name, templatepath, contentpath):
 	with open(templatepath, "r") as template:
 		htmltemplate = template.readlines()
 		with open(contentpath, "r") as content:
 			htmlcontent = content.readlines()
 			with open(name, "a") as final:
-				final.write(htmltemplate)
-				final.write(htmlcontent)
+				final.writelines(htmltemplate)
+				final.writelines(htmlcontent)
 
+
+def main():
+	name = sys.argv[1]
+	template = sys.argv[2]
+	content = sys.argv[3]
+	writetofile(name, template, content)
+
+
+def winmain():
+	name = input("Please enter name of the final file: ")
+	template = input("Enter templatepath: ")
+	content = input("Enter contentpath")
+	writetofile(name, template, content)
 
 
 if __name__ == "__main__":
