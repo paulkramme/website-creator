ec0d0f491b6826048a8765f59f70fba6e47a7cb3
Paul Kramme <pjkramme@gmail.com>
2016-12-01 15:50:36 +0100

    Add configurator html and css

--- wscreator.py
+++ wscreator.py
@@ -4,12 +4,33 @@ import os
 import sys
 
 
-def configurator():
+def configurator(source, filename = "final.html"):
 	author = input("Author? ")
 	title = input("Title? ")
 	description = input("Description? ")
 	url = input("URL? ")
-	
+	with open(filename, "w") as f:
+		f.write("<!doctype html>\n")
+		f.write("<html>\n")
+		f.write('\t<meta charset="utf-8">\n')
+		f.write('\t<meta name="viewport" content="width=device-width">\n')
+		f.write('\t<meta name="author" content="' + author + '">\n')
+		f.write('\t<meta name="description" content="' + description + '">\n')
+		f.write('\t<meta property="og:url" content="' + url + '">\n')
+		f.write('\t<meta property="og:title" content="' + title + '">\n')
+		f.write('\t<meta property="og:site_name" content="' + title + '">\n')
+		f.write('\t<meta property="og:description" content="' + description + '">\n')
+		f.write('\t<meta name="twitter:card" content="summary">\n')
+		f.write('\t<meta name="twitter:title" content="' + title + '">\n')
+		f.write('\t<meta name="twitter:description" content="' + description + '">\n')
+		f.write('\t<meta name="twitter:creator" content="' + author + '">\n')
+		f.write('\t<title>' + title + '</title>\n')
+		f.write('\t<style type="text/css">body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:010px}h1,h2,h3{line-height:1.2}</style>\n')
+		f.write('</head>')
+	with open(filename, "a") as final:
+		with open(source, "r") as temp:
+			lines = temp.readlines()
+			final.writelines(lines)
 
 
 def writetofile(name, templatepath, contentpath):
@@ -23,17 +44,14 @@ def writetofile(name, templatepath, contentpath):
 
 
 def main():
-	name = sys.argv[1]
-	template = sys.argv[2]
-	content = sys.argv[3]
-	writetofile(name, template, content)
+	configurator(sys.argv[1], sys.argv[2])
 
 
 def winmain():
 	name = input("Please enter name of the final file: ")
-	template = input("Enter templatepath: ")
 	content = input("Enter contentpath")
-	writetofile(name, template, content)
+	#writetofile(name, template, content)
+	configurator(content, name)
 
 
 if __name__ == "__main__":
